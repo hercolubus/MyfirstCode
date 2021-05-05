8963dd124c86f94668fc3869e69c09e3fc036ff5
hercolubus <hector.meneses.aguilera@gmail.com>
2021-05-05 02:25:45 -0400

    descripcion variables

--- variables.tf
+++ variables.tf
@@ -1,18 +1,18 @@
 variable "subscription_id" {
   description = "Suscription ID"
   type        = string
-  default     = "Change Me"
+  default     = "Change Me" // cambiar valor por el suscription id
   sensitive   = true
 }
 
 variable "tag" {
   description = "tag"
   type        = string
-  default     = "Grupo DevOps"
+  default     = "Grupo DevOps" // cambiar valor por un tag único ejemplo Grupo DevOps C
 }
 
 variable "rsgroup" {
   description = "Resource Group"
   type        = string
-  default     = "DevOps"
+  default     = "DevOps" // cambiar valor por un grupo de recursos único ejemplo DevOps C
 }
